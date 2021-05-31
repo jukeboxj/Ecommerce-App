030a474cfb95a5016549d758908b2cb9b54120f6
jukeboxj <skyyy.chen@gmail.com>
2021-05-31 00:19:54 -0400

    Updated Cart Count
    from count of types to total count

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,40 +1,41 @@
-import React, {useState, useEffect} from 'react';
-import {connect} from 'react-redux';
-import {NavLink} from 'react-router-dom';
-import './Header.scss';
+import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import './Header.scss'
 
-const Header = ({cartLength, shakeCart}) => {
-
+const Header = ({ cartCount, shakeCart }) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div className="container">
-                <NavLink className="navbar-brand" to="/">Kirkfall</NavLink>
-                <div className={shakeCart? 'wiggle' : ''}>
+                <NavLink className="navbar-brand" to="/">
+                    Kirkfall
+                </NavLink>
+                <div className={shakeCart ? 'wiggle' : ''}>
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item">
-                            <NavLink className="nav-link" to={"/cart"}>
-                                <i className="bi bi-cart-fill me-2"
-                                    aria-hidden="true"/>
-                                Cart {cartLength ? `(${cartLength})`: ''}
+                            <NavLink className="nav-link" to={'/cart'}>
+                                <i
+                                    className="bi bi-cart-fill me-2"
+                                    aria-hidden="true"
+                                />
+                                Cart {cartCount ? `(${cartCount})` : ''}
                             </NavLink>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
-    );
-};
+    )
+}
 
+const mapStateToProps = state => {
+    return {
+        cartCount: state.shop.cart.reduce((a, b) => a + b.quantity, 0),
+        shakeCart: state.shop.shakeCart,
+    }
+}
 
-const mapStateToProps = (state) => {
-  return {
-        cartLength: state.shop.cart.length,
-        shakeCart: state.shop.shakeCart
-  }
-};
-
-export default connect(mapStateToProps, null)(Header);
-
+export default connect(mapStateToProps, null)(Header)
 
 /*
 *                         <li className="nav-item active">
