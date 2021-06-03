c80d5d5b524c91d81939776d63de4f07fba40b23
jukeboxj <skyyy.chen@gmail.com>
2021-06-03 18:54:59 -0400

    updated carousel styling

--- frontend/src/components/ProductSlider/ProductSlider.js
+++ frontend/src/components/ProductSlider/ProductSlider.js
@@ -16,74 +16,83 @@ const ProductSlider = ({ product }) => {
 
     return (
         <aside className="col-sm-5 border-right">
-            <div style={{ padding: '2rem' }}>
-                <div
-                    id="carousel"
-                    className="carousel carousel-dark slide"
-                    data-bs-ride="carousel"
-                >
-                    <div className="carousel-indicators">
-                        {images.map((img, i) => {
-                            return (
-                                <button
-                                    type="button"
-                                    data-bs-target="#carousel"
-                                    data-bs-slide-to={i}
-                                    className="active"
-                                    aria-current="true"
-                                    aria-label={`Slide ${i}`}
-                                    key={img}
-                                ></button>
-                            )
-                        })}
-                    </div>
-                    <div className="carousel-inner">
-                        {images.map((img, i) => {
-                            const classname = classNames('carousel-item', {
-                                active: i === 0,
-                            })
+            <article className="gallery-wrap">
+                <div className="img-big-wrap">
+                    <div style={{ padding: '1rem' }}>
+                        <div
+                            id="carousel"
+                            className="img-small-wrap carousel carousel-dark slide"
+                            data-bs-ride="carousel"
+                        >
+                            <div className="carousel-indicators">
+                                {images.map((img, i) => {
+                                    return (
+                                        <button
+                                            type="button"
+                                            data-bs-target="#carousel"
+                                            data-bs-slide-to={i}
+                                            className="active"
+                                            aria-current="true"
+                                            aria-label={`Slide ${i}`}
+                                            key={img}
+                                        ></button>
+                                    )
+                                })}
+                            </div>
+                            <div className="carousel-inner">
+                                {images.map((img, i) => {
+                                    const classname = classNames(
+                                        'carousel-item',
+                                        {
+                                            active: i === 0,
+                                        }
+                                    )
 
-                            return (
-                                <div
-                                    className={classname}
-                                    data-bs-interval="2500"
-                                    key={img}
-                                >
-                                    <img
-                                        src={img}
-                                        className="d-block w-100"
-                                        alt={i}
-                                    />
-                                </div>
-                            )
-                        })}
+                                    return (
+                                        <div
+                                            className={classname}
+                                            data-bs-interval="2500"
+                                            key={img}
+                                        >
+                                            <img
+                                                src={img}
+                                                className="d-block w-100"
+                                                alt={i}
+                                            />
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                            <button
+                                className="carousel-control-prev"
+                                type="button"
+                                data-bs-target="#carousel"
+                                data-bs-slide="prev"
+                            >
+                                <span
+                                    className="carousel-control-prev-icon"
+                                    aria-hidden="true"
+                                ></span>
+                                <span className="visually-hidden">
+                                    Previous
+                                </span>
+                            </button>
+                            <button
+                                className="carousel-control-next"
+                                type="button"
+                                data-bs-target="#carousel"
+                                data-bs-slide="next"
+                            >
+                                <span
+                                    className="carousel-control-next-icon"
+                                    aria-hidden="true"
+                                ></span>
+                                <span className="visually-hidden">Next</span>
+                            </button>
+                        </div>
                     </div>
-                    <button
-                        className="carousel-control-prev"
-                        type="button"
-                        data-bs-target="#carousel"
-                        data-bs-slide="prev"
-                    >
-                        <span
-                            className="carousel-control-prev-icon"
-                            aria-hidden="true"
-                        ></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button
-                        className="carousel-control-next"
-                        type="button"
-                        data-bs-target="#carousel"
-                        data-bs-slide="next"
-                    >
-                        <span
-                            className="carousel-control-next-icon"
-                            aria-hidden="true"
-                        ></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
                 </div>
-            </div>
+            </article>
         </aside>
     )
 }
