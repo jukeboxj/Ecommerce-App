cf36143d3ca591cf31eefec7687c5be75e67f865
jukeboxj <skyyy.chen@gmail.com>
2022-11-07 22:28:36 -0500

    upgrades
    - react router dom upgrade to v6
    - nav bar to Material UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Header from './components/Header/Header'
@@ -9,28 +9,25 @@ import ShoppingCart from './pages/ShoppingCart'
 
 const App = () => {
     return (
-        <>
-            <BrowserRouter>
-                <React.Fragment>
-                    <Header />
-                    <Switch>
-                        <Route exact path={'/products'} component={Home} />
-                        <Route
-                            exact
-                            path={'/products/:_id'}
-                            component={ProductDetails}
-                        />
-                        <Route exact path={'/cart'} component={ShoppingCart} />
-                        <Route
-                            render={() => {
-                                return <Redirect push to={'/products'} />
-                            }}
-                        />
-                    </Switch>
-                    <Footer />
-                </React.Fragment>
-            </BrowserRouter>
-        </>
+        <BrowserRouter>
+            <React.Fragment>
+                <Header />
+                <Routes>
+                    <Route exact path="/products" element={<Home />} />
+                    <Route
+                        exact
+                        path="/products/:_id"
+                        element={<ProductDetails />}
+                    />
+                    <Route exact path="/cart" element={<ShoppingCart />} />
+                    <Route
+                        path="*"
+                        element={<Navigate to="/products" replace />}
+                    />
+                </Routes>
+                <Footer />
+            </React.Fragment>
+        </BrowserRouter>
     )
 }
 
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Menu from '@mui/material/Menu'
+import MenuIcon from '@mui/icons-material/Menu'
+import Container from '@mui/material/Container'
+import Button from '@mui/material/Button'
+import Tooltip from '@mui/material/Tooltip'
+import MenuItem from '@mui/material/MenuItem'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
 import { useSelector } from 'react-redux'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import className from 'classnames'
 import './Header.scss'
+
+const pages = ['FAQ', 'About']
 
 const Header = () => {
     const shakeCart = useSelector(state => state.shop.shakeCart)
@@ -13,27 +28,156 @@ const Header = () => {
 
     const wiggle = className('nav-item', { wiggle: shakeCart })
 
+    const [anchorElNav, setAnchorElNav] = React.useState(null)
+    const [anchorElUser, setAnchorElUser] = React.useState(null)
+
+    const handleOpenNavMenu = event => {
+        setAnchorElNav(event.currentTarget)
+    }
+    const handleOpenUserMenu = event => {
+        setAnchorElUser(event.currentTarget)
+    }
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null)
+    }
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null)
+    }
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top py-1">
-            <div className="container">
-                <NavLink className="navbar-brand" to={'/'}>
-                    Kirkfall
-                </NavLink>
-                <div>
-                    <ul className="navbar-nav ms-auto">
-                        <li className={wiggle} key="cart">
-                            <NavLink className="nav-link" to={'/cart'}>
-                                <i
-                                    className="bi bi-cart-fill me-2"
-                                    aria-hidden="true"
-                                />
-                                Cart{count > 0 ? ` (${count})` : ''}
-                            </NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <AppBar
+            position="static"
+            sx={{
+                marginBottom: 2,
+            }}
+        >
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'Noto Sans',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Kirkfall
+                    </Typography>
+
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: 'flex', md: 'none' },
+                        }}
+                    >
+                        <IconButton
+                            size="large"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map(page => (
+                                <MenuItem
+                                    key={page}
+                                    onClick={handleCloseNavMenu}
+                                >
+                                    <Typography textAlign="center">
+                                        {page}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'Noto Sans',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Kirkfall
+                    </Typography>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: 'none', md: 'flex' },
+                        }}
+                    >
+                        {pages.map(page => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{
+                                    my: 2,
+                                    color: 'white',
+                                    display: 'block',
+                                }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }} className={wiggle}>
+                        <Tooltip title="Open cart">
+                            <Link to="/cart">
+                                <Button
+                                    className="nav-link"
+                                    sx={{
+                                        color: 'white',
+                                    }}
+                                >
+                                    <ShoppingCartIcon />
+                                    <span>
+                                        {'  '}
+                                        Cart {count > 0 ? ` (${count})` : ''}
+                                    </span>
+                                </Button>
+                            </Link>
+                        </Tooltip>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     )
 }
 
